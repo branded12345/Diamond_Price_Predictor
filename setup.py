d6b0cfc1cbee2cfdcb2c1f0bf813ab651d69d460
branded1234 <brandedoriginal01@gmail.com>
2024-02-24 21:13:10 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 from setuptools import find_packages,setup
 from typing import List
 
-def get_requirements(file_path:str)-> List[str]:
+# HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
-        requirements= file_obj.readlines()
-        requirements= [req.replace("\n", "") for req in requirements]
-        return requirements
-    
-setup(
-    name= 'DiamondPricePrediction',
-    version= '0.01',
-    author= 'Keshav',
-    author_email= 'keshavsharma6173@gmail.com',
-    install_requires= get_requirements('requirements.txt'),
-    packages= find_packages()
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-)
+        # if HYPEN_E_DOT in requirements:
+        #     requirements.remove(HYPEN_E_DOT)
+
+        return requirements
+
+
+setup(
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='keshav',
+    author_email='keshavsharma6173@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+
+) 
